c05075e4c3c9532136ed80944059bd148a89d4a3
Paul <paul@datama.fr>
2025-10-24 12:40:08 +0200

    new batch function for larger dataset

--- train.py
+++ train.py
@@ -1,50 +1,41 @@
 import torch
+import numpy as np
+
+from tokenizer import truncated_cl100k
 from model.gpt import GPTLanguageModel
 
 
 # hyperparameters
+num_merges = 16000 # number of merges for the tokenizer
 batch_size = 64 # how many independent sequences will we process in parallel?
-block_size = 256 # what is the maximum context length for predictions?
+block_size = 512 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 384
-n_head = 6
-n_layer = 6
-dropout = 0.2
+n_embd = 512
+n_head = n_embd // 64
+n_layer = 8
+dropout = 0.1
 # ------------
 
 torch.manual_seed(1337)
 
-# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('data/input.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
+# train and test data
+train_data = np.memmap(f"data/train.bin", dtype=np.uint16, mode="r+")
+val_data = np.memmap(f"data/val.bin", dtype=np.uint16, mode="r+")
 
-# here are all the unique characters that occur in this text
-chars = sorted(list(set(text)))
-vocab_size = len(chars)
-# create a mapping from characters to integers
-stoi = { ch:i for i,ch in enumerate(chars) }
-itos = { i:ch for i,ch in enumerate(chars) }
-encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
-
-# Train and test splits
-data = torch.tensor(encode(text), dtype=torch.long)
-n = int(0.9*len(data)) # first 90% will be train, rest val
-train_data = data[:n]
-val_data = data[n:]
-
-# data loading
 def get_batch(split):
     # generate a small batch of data of inputs x and targets y
-    data = train_data if split == 'train' else val_data
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i+block_size] for i in ix])
-    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-    x, y = x.to(device), y.to(device)
+    data = train_data if split == "train" else val_data
+    arr = np.asarray(data)
+    L = arr.shape[0]
+    idx = torch.randint(low=0, high=L - block_size - 1, size=(batch_size,))
+    x = torch.stack([torch.from_numpy(arr[i:i+block_size]) for i in idx])
+    y = torch.stack([torch.from_numpy(arr[i+1:i+block_size+1]) for i in idx])
+    x = x.to(device=device, dtype=torch.long, non_blocking=True)
+    y = y.to(device=device, dtype=torch.long, non_blocking=True)
     return x, y
 
 @torch.no_grad()
@@ -61,7 +52,8 @@ def estimate_loss():
     model.train()
     return out
 
-
+tokenizer = truncated_cl100k(16000)
+vocab_size = tokenizer.n_vocab
 
 model = GPTLanguageModel(
     vocab_size=vocab_size,
@@ -80,6 +72,7 @@ print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
 # create a PyTorch optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
+
 for iter in range(max_iters):
 
     # every once in a while evaluate the loss on train and val sets
@@ -95,8 +88,3 @@ for iter in range(max_iters):
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
-
-# generate from the model
-context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
-#open('more.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
